260d12e618b6d68d6c08e3562ffc54a06bf81ee9
JonathanCG22 <jonhyacg2@gmail.com>
2023-05-25 10:52:13 -0600

    Revert "Version Beta Chacon 0.1"
    This reverts commit f8fadfadb31e531c5973e0a4d77331cd7f2fceee.

--- modules/Renta/datos.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../../config/conexion.php';
-$crud = new CRUD;
-$costo=$_POST['costo'];
-
-echo "<input type='text' name='total' class='form-control' value=".$costo." required='required' maxlength='30' disable>";
-        
-      
-?>
